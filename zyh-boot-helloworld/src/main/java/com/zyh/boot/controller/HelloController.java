d528c2d87c6a2386bc03b21ed6269f6ac9dccb16
zheng-yuhan <z_1995@yeah.net>
2020-12-16 00:21:48 +0800

    first commit

---
+++ zyh-boot-helloworld/src/main/java/com/zyh/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zyh.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String home(){
+        return "hello ";
+    }
+}
